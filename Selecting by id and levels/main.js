19ba4d2918aa2edb910e48ee61e69df30206649d
Emmanuel Twum Osafo <72449645+EmmanuelTwum@users.noreply.github.com>
2020-10-09 23:47:56 +0000

    Add files via upload

---
+++ Selecting by id and levels/main.js
@@ -0,0 +1,11 @@
+//selecting by id and levels
+$(document).ready( () =>{
+    $('#container >>>.non-solid').addClass("highlight");
+});
+
+/*
+//selecting by class
+$(document).ready( () =>{
+    $('.non-solid').addClass("highlight");
+});
+*/
